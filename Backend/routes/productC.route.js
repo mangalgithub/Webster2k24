bb8cfb822cafe556e746c06a5d4f12a19d1b6e31
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2024-11-01 14:45:51 +0530

    fixed in route

--- Backend/routes/productC.route.js
+++ Backend/routes/productC.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { showAllProducts, showProductById,searchProductByName } from './../controllers/productC.controller.js';
+import { showAllProducts, showProductById,searchProductByName,topFiveProducts } from './../controllers/productC.controller.js';
 const router = express.Router();
 
 router.route("/showAllProducts").get(showAllProducts);
 router.route("/showProductById:productId").get(showProductById);
 router.route("/searchProductByName").get(searchProductByName);
+router.route("/topFiveProducts").get(topFiveProducts);
 export default router;
